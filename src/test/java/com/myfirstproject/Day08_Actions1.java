05960cb5fea77df258cbe9cc3a3ff23c42ff3c9f
mytechproed1 <mytechproed@gmail.com>
2023-05-23 10:22:58 -0500

    day15

--- src/test/java/com/myfirstproject/Day08_Actions1.java
+++ src/test/java/com/myfirstproject/Day08_Actions1.java
@@ -1,5 +1,6 @@
 package com.myfirstproject;
 
+import com.myfirstproject.utilities.LoggerUtils;
 import com.myfirstproject.utilities.TestBase;
 import org.junit.Assert;
 import org.junit.Test;
@@ -11,18 +12,21 @@ public class Day08_Actions1 extends TestBase {
 
     @Test
     public void contextClickTest(){
+        LoggerUtils.info("Opening the URL");
         driver.get("https://testcenter.techproeducation.com/index.php?page=context-menu");
 //        When use Right clicks on the box
+        LoggerUtils.info("Right click on hte box");
         WebElement box = driver.findElement(By.id("hot-spot"));
 
 //        Creating Actions Class
         Actions actions = new Actions(driver);//creating Actions object
         actions.contextClick(box).perform();//right click on the box
-
+        LoggerUtils.info("Assert alert message");
 //        Then verify the alert message is “You selected a context menu”
         Assert.assertTrue(driver.switchTo().alert().getText().equals("You selected a context menu"));
 //        Then accept the alert
         driver.switchTo().alert().accept();
+        LoggerUtils.info("Testing SUCCESSFUL");
 
     }
 
---
+++ src/test/java/com/myfirstproject/Day15_Log4J2.java
@@ -0,0 +1,25 @@
+package com.myfirstproject;
+
+import com.myfirstproject.utilities.LoggerUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Test;
+
+public class Day15_Log4J2 {
+    private static Logger logger = LogManager.getLogger(Day15_Log4J2.class.getName());
+    @Test
+    public void loggerTest1(){
+        logger.fatal("Fatal log .... ");
+        logger.error("Error log ... ");
+        logger.warn("Warning log ... ");//NOT YET. NOW AFTER log4j2.xml WE SEE THIS LEVEL
+        logger.debug("Debug log ... ");//NOT YET. NOW AFTER log4j2.xml WE SEE THIS LEVEL
+        logger.info("Info log ... ");//NOT YET. NOW AFTER log4j2.xml WE SEE THIS LEVEL
+    }
+
+    @Test
+    public void loggerTest2(){
+        LoggerUtils.fatal("Fatal log");
+        LoggerUtils.info("Info Log");
+        LoggerUtils.error("Error Log");
+    }
+}
